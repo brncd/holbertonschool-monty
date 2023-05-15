ed67c4a4bb5fbecd12ff200052a124b5b02e8396
brncd <brunocarnales@gmail.com>
2023-05-15 06:34:56 -0700

    updated header

--- monty.h
+++ monty.h
@@ -36,4 +36,11 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void push(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void (*get_opcode_function(char *opcode))(stack_t **stack, unsigned int line_number);
+
 #endif
