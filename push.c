f2c5c17895f86aa10ac8bddf4b86241149226560
brncd <brunocarnales@gmail.com>
2023-05-15 07:05:21 -0700

    betty

--- push.c
+++ push.c
@@ -7,39 +7,39 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    char* numchar = NULL;
-    int num = 0;
-    stack_t *newNode = NULL;
+	char *numchar = NULL;
+	int num = 0;
+	stack_t *newNode = NULL;
 
-    numchar = strtok(NULL, " \t\n");
-    if (!numchar)
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit_cleanup(EXIT_FAILURE, stack, newNode);
-    }
+	numchar = strtok(NULL, " \t\n");
+	if (!numchar)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit_cleanup(EXIT_FAILURE, stack, newNode);
+	}
 
-    char* endptr;
-    num = strtol(numchar, &endptr, 10);
-    if (*endptr != '\0')
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit_cleanup(EXIT_FAILURE, stack, newNode);
-    }
+	char *endptr;
+	num = strtol(numchar, &endptr, 10);
+	if (*endptr != '\0')
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit_cleanup(EXIT_FAILURE, stack, newNode);
+	}
 
-    newNode = malloc(sizeof(stack_t));
-    if (!newNode)
-    {
-        fprintf(stderr, "Error: unable to allocate memory\n");
-        exit_cleanup(EXIT_FAILURE, stack, newNode);
-    }
+	newNode = malloc(sizeof(stack_t));
+	if (!newNode)
+	{
+		fprintf(stderr, "Error: unable to allocate memory\n");
+		exit_cleanup(EXIT_FAILURE, stack, newNode);
+	}
 
-    newNode->n = num;
-    newNode->next = *stack;
-    newNode->prev = NULL;
+	newNode->n = num;
+	newNode->next = *stack;
+	newNode->prev = NULL;
 
-    if (*stack != NULL)
-    {
-        (*stack)->prev = newNode;
-    }
-    *stack = newNode;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = newNode;
+	}
+	*stack = newNode;
 }
